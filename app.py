c2f31df7020fc985782c026db9cc5a2617f54624
Justin Stewart <justinstewart.la@gmail.com>
2017-03-12 14:08:28 -0400

    fixing port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 import models
 from resources.courses import courses_api
@@ -6,7 +7,7 @@ from resources.reviews import reviews_api
 
 DEBUG = True
 HOST = '0.0.0.0'
-PORT = 33507
+PORT = int(os.environ.get('PORT',5000))
 
 app = Flask(__name__)
 app.register_blueprint(courses_api,url_prefix='/api/v1')
